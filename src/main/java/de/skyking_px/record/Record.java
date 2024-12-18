797387ce1b76a6cddb9adf67bd6cd6bcee28a361
Antony Blem <antony.blem@icloud.com>
2024-12-18 13:54:31 +0100

    Integrate LuckPerms API

--- src/main/java/de/skyking_px/record/Record.java
+++ src/main/java/de/skyking_px/record/Record.java
@@ -1,22 +1,39 @@
 package de.skyking_px.record;
 
+import net.luckperms.api.LuckPerms;
+import net.luckperms.api.node.Node;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Color;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class Record extends JavaPlugin implements CommandExecutor {
 
+    RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+
     @Override
     public void onEnable() {
         // Plugin startup logic
-        getLogger().info("[Record] Loading Plugin...");
-        getLogger().info("[Record] By SkyKing_PX | Version: 1.21.1-1.0.0");
+        getLogger().info("[Record] Loading Plugin...\n[Record] By SkyKing_PX | Version: 1.21-1.0.0\n[Record] Please make sure to create LuckPerms Groups named 'rec' and 'live'!");
         this.getCommand("rec").setExecutor(this);
         this.getCommand("rec").setTabCompleter(this);
+        this.getCommand("live").setExecutor(this);
+        this.getCommand("live").setTabCompleter(this);
+
+        if (provider != null) {
+            LuckPerms luckPerms = provider.getProvider();
+        } else {
+            getLogger().severe("LuckPerms API not found, disabling!");
+            Bukkit.getPluginManager().disablePlugin(this);
+        }
+
     }
 
     @Override
@@ -27,15 +44,39 @@ public final class Record extends JavaPlugin implements CommandExecutor {
 
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender.hasPermission("rec.rec")){
-            if (sender instanceof Player){
-                Player p = (Player) sender;
-                if (p.getDisplayName().contains("Recording")){
-                    
-                } else p.setDisplayName((Color.PURPLE + "[") + (Color.RED + "Recording") + (Color.PURPLE + "]") + " " + p.getName());
+        if (sender instanceof Player) {
+            Player p = (Player) sender;
+            LuckPerms luckPerms = provider.getProvider();
+            if (sender.hasPermission("lsrec.rec")) {
 
-            } else sender.sendMessage(ChatColor.RED + "Bitte führe diesen Befehl als Spieler aus!");
-        } else sender.sendMessage(ChatColor.RED + "Du hast nicht die Berechtigung diesen Befehl auszuführen!");
+                if (p.hasPermission("group.rec")) {
+                    luckPerms.getUserManager().modifyUser(p.getUniqueId(), user -> {
+                        user.data().remove(Node.builder("group.rec").build());
+                    });
+                    getLogger().info("[Record] Added status '[Rec]' for " + p.getName());
+                } else {
+                    luckPerms.getUserManager().modifyUser(p.getUniqueId(), user -> {
+                        user.data().add(Node.builder("group.rec").build());
+                    });
+                    getLogger().info("[Record] Removed status '[Rec]' for " + p.getName());
+                }
+
+            } else if (sender.hasPermission("lsrec.live")) {
+
+                if (p.hasPermission("group.live")) {
+                    luckPerms.getUserManager().modifyUser(p.getUniqueId(), user -> {
+                        user.data().remove(Node.builder("group.live").build());
+                    });
+                    getLogger().info("[Record] Added status '[Live]' for " + p.getName());
+                } else {
+                    luckPerms.getUserManager().modifyUser(p.getUniqueId(), user -> {
+                        user.data().add(Node.builder("group.live").build());
+                    });
+                    getLogger().info("[Record] Removed status '[Live]' for " + p.getName());
+                }
+
+            } else sender.sendMessage(ChatColor.RED + "Du hast nicht die Berechtigung diesen Befehl auszuführen!");
+        } else sender.sendMessage(ChatColor.RED + "Bitte führe diesen Befehl als Spieler aus!");
         return true;
     }
 }
