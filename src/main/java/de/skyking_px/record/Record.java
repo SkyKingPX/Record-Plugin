abce5bbc610d716ce7a8f2ab95321edb4e43a28b
Antony Blem <antony.blem@icloud.com>
2024-12-19 15:28:23 +0100

    Use logout Event instead of reset-on-shutdown (Todo: Prefixes don't work from the config)

--- src/main/java/de/skyking_px/record/Record.java
+++ src/main/java/de/skyking_px/record/Record.java
@@ -1,6 +1,7 @@
 package de.skyking_px.record;
 
 import net.luckperms.api.LuckPerms;
+import net.luckperms.api.event.EventBus;
 import net.luckperms.api.model.group.Group;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.node.Node;
@@ -8,15 +9,20 @@ import net.luckperms.api.node.types.PermissionNode;
 import net.luckperms.api.node.types.PrefixNode;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.Color;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.checkerframework.checker.nullness.qual.NonNull;
 import org.checkerframework.checker.nullness.qual.Nullable;
+import org.checkerframework.checker.units.qual.Prefix;
 
 import java.io.File;
 import java.io.IOException;
@@ -24,7 +30,7 @@ import java.nio.file.Files;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 
-public final class Record extends JavaPlugin implements CommandExecutor {
+public final class Record extends JavaPlugin implements CommandExecutor, Listener {
 
     @Override
     public void onEnable() {
@@ -48,8 +54,9 @@ public final class Record extends JavaPlugin implements CommandExecutor {
                 e.printStackTrace();
             }
         }
+//        Not updatet anymore as it is WIP
 //        else {
-//            @Nullable String[] valueCheck = {String.valueOf(getConfig().getBoolean("reset-on-shutdown")), String.valueOf(getConfig().getBoolean("delete-groups-on-shutdown")),
+//            @Nullable String[] valueCheck = {String.valueOf(getConfig().getBoolean("reset-on-logout")), String.valueOf(getConfig().getBoolean("delete-groups-on-shutdown")),
 //                    getConfig().getString("rec-prefix"), getConfig().getString("live-prefix"), String.valueOf(getConfig().getInt("rec-prefix-weight")),
 //                    String.valueOf(getConfig().getInt("live-prefix-weight")), getConfig().getString("messages.console.invalid-config"),
 //                    getConfig().getString("messages.console.rec-group-already-exists"),
@@ -82,6 +89,7 @@ public final class Record extends JavaPlugin implements CommandExecutor {
         RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
         if (provider != null) {
             LuckPerms luckPerms = provider.getProvider();
+            getLogger().info("LuckPerms API found! Version: " + luckPerms.getPluginMetadata().getVersion());
         } else {
             getLogger().severe("LuckPerms API not found, disabling!");
             Bukkit.getPluginManager().disablePlugin(this);
@@ -94,7 +102,7 @@ public final class Record extends JavaPlugin implements CommandExecutor {
         } else {
             @NonNull CompletableFuture<Group> groupFuture = luckPerms.getGroupManager().createAndLoadGroup("rec");
             groupFuture.thenAccept(group -> {
-                PrefixNode prefixNode = PrefixNode.builder(getConfig().getString("rec-prefix"), getConfig().getInt("rec-prefix-weight")).build();
+                PrefixNode prefixNode = PrefixNode.builder(Objects.requireNonNull(getConfig().getString("rec-prefix")), getConfig().getInt("rec-prefix-weight")).build();
                 PermissionNode permissionNode = PermissionNode.builder("group.rec").value(true).build();
                 group.data().add(prefixNode);
                 group.data().add(permissionNode);
@@ -113,7 +121,7 @@ public final class Record extends JavaPlugin implements CommandExecutor {
         } else {
             @NonNull CompletableFuture<Group> groupFuture = luckPerms.getGroupManager().createAndLoadGroup("live");
             groupFuture.thenAccept(group -> {
-                PrefixNode prefixNode = PrefixNode.builder(getConfig().getString("live-prefix"), getConfig().getInt("live-prefix-weight")).build();
+                PrefixNode prefixNode = PrefixNode.builder(Objects.requireNonNull(getConfig().getString("live-prefix")), getConfig().getInt("live-prefix-weight")).build();
                 PermissionNode permissionNode = PermissionNode.builder("group.live").value(true).build();
                 group.data().add(prefixNode);
                 group.data().add(permissionNode);
@@ -131,44 +139,19 @@ public final class Record extends JavaPlugin implements CommandExecutor {
     public void onDisable() {
         // Plugin shutdown logic
         getLogger().info("Disabling Plugin...");
-        if (getConfig().getBoolean("reset-on-shutdown")) {
-            for (Player p : Bukkit.getOnlinePlayers()) {
-                RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
-                LuckPerms luckPerms = Objects.requireNonNull(provider).getProvider();
-                User user = luckPerms.getUserManager().getUser(p.getUniqueId());
-                if (p.hasPermission("group.rec")) {
-                    removeGroup("rec", user, luckPerms, p);
-                } else if (p.hasPermission("group.live")) {
-                    removeGroup("live", user, luckPerms, p);
-                }
+        if (getConfig().getBoolean("delete-groups-on-shutdown")) {
+            RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+            LuckPerms luckPerms = provider.getProvider();
+            Group recGroup = luckPerms.getGroupManager().getGroup("rec");
+            Group liveGroup = luckPerms.getGroupManager().getGroup("live");
+            if (recGroup != null) {
+                luckPerms.getGroupManager().deleteGroup(recGroup);
+                getLogger().info(getConfig().getString("messages.console.deleted-group-rec"));
             }
-//            for (OfflinePlayer p : Bukkit.getOfflinePlayers()) {
-//                RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
-//                LuckPerms luckPerms = Objects.requireNonNull(provider).getProvider();
-//                User user = luckPerms.getUserManager().getUser(p.getUniqueId());
-//                if (Objects.requireNonNull(p.getPlayer()).hasPermission("group.rec")) {
-//                    removeGroupOffline("rec", user, luckPerms, p);
-//                } else if (Objects.requireNonNull(p.getPlayer()).hasPermission("group.live")) {
-//                    removeGroupOffline("live", user, luckPerms, p);
-//                }
-//            }
-            getLogger().info(getConfig().getString("messages.console.reset-players"));
-
-            if (getConfig().getBoolean("delete-groups-on-shutdown")) {
-                RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
-                LuckPerms luckPerms = provider.getProvider();
-                Group recGroup = luckPerms.getGroupManager().getGroup("rec");
-                Group liveGroup = luckPerms.getGroupManager().getGroup("live");
-                if (recGroup != null) {
-                    luckPerms.getGroupManager().deleteGroup(recGroup);
-                    getLogger().info(getConfig().getString("messages.console.deleted-group-rec"));
-                }
-                if (liveGroup != null) {
-                    luckPerms.getGroupManager().deleteGroup(liveGroup);
-                    getLogger().info(getConfig().getString("messages.console.deleted-group-live"));
-                }
+            if (liveGroup != null) {
+                luckPerms.getGroupManager().deleteGroup(liveGroup);
+                getLogger().info(getConfig().getString("messages.console.deleted-group-live"));
             }
-
         }
     }
 
@@ -211,6 +194,22 @@ public final class Record extends JavaPlugin implements CommandExecutor {
             } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.player.insufficient-perms")));
         } else sender.sendMessage(ChatColor.translateAlternateColorCodes('&', getConfig().getString("messages.player.please-run-as-player")));
         return true;
+    }
+
+    @EventHandler
+    public void onLogout(PlayerQuitEvent event){
+        if (getConfig().getBoolean("reset-on-logout")) {
+            Player p = event.getPlayer();
+            RegisteredServiceProvider<LuckPerms> provider = Bukkit.getServicesManager().getRegistration(LuckPerms.class);
+            LuckPerms luckPerms = provider.getProvider();
+            User user = luckPerms.getUserManager().getUser(p.getUniqueId());
+            if (p.hasPermission("group.rec")) {
+                removeGroup("rec", user, luckPerms, p);
+            } else if (p.hasPermission("group.live")) {
+                removeGroup("live", user, luckPerms, p);
+            }
+            getLogger().info(getConfig().getString("messages.console.reset-player") + p.getName());
+        }
     }
 
     public void addGroup(String groupName, User user, LuckPerms luckPerms, Player p) {
